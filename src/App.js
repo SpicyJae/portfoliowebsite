bbed401261d3fcd18a11fa7f1959b55f7ed895c8
SpicyJae <rlawogus9643@gmail.com>
2021-08-07 21:26:39 -0700

    added loading screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Preloader } from './Preloader';
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2000)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>{
+      !loading ? (
+        <div className="App">
+          <header className="App-header">
+            loaded
+          </header>
+        </div>
+      ) : (
+        <Preloader/>
+      )
+    }
+    </>
+
   );
 }
 
---
+++ src/Preloader.js
@@ -0,0 +1,13 @@
+
+import "./Preloader.css";
+
+export function Preloader() {
+  return (
+            <div className="spinner">
+              <span>Loading...</span>
+              <div className="half-spinner"></div>
+            </div>
+            
+          ); 
+          <h1>Your Data</h1>
+}
